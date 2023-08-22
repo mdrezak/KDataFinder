910c1bf0b13ba31f7fabe1df26ec4ea2b656d010
Mr K <md.reza.k.rg@gmail.com>
2023-08-22 22:37:16 +0330

    Implement base Task manager

---
+++ KDataFinder.ConsoleApp/Implementation/TaskManager.cs
@@ -0,0 +1,30 @@
+﻿using KDataFinder.ConsoleApp.Abstraction;
+
+namespace KDataFinder.ConsoleApp.Implementation;
+
+internal class TaskManager
+{
+    public int MaximumConcurrentTasks { get; set; }
+    public Task[] ConcurrentTasks { get; }
+
+    public TaskManager(int maximumConcurrentTasks)
+    {
+        MaximumConcurrentTasks = maximumConcurrentTasks;
+        ConcurrentTasks = new Task[maximumConcurrentTasks];
+    }
+
+    public Task AddTask(Task task)
+    {
+        if (ConcurrentTasks.Count(x => x == null) > 0)
+        {
+            ConcurrentTasks[Array.IndexOf(ConcurrentTasks, null)] = (task);
+        }
+        else
+        {
+            var completedTaskIndex = Task.WaitAny(ConcurrentTasks);
+            ConcurrentTasks[completedTaskIndex] = task;
+        }
+        return Task.CompletedTask;
+    }
+
+}
